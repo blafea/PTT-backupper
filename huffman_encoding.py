df007dde0880eda55596f05efbd672f3b1423571
blafea <86761168+blafea@users.noreply.github.com>
2022-01-07 20:53:29 +0800

    Update huffman_encoding.py

--- huffman_encoding.py
+++ huffman_encoding.py
@@ -2,17 +2,20 @@ import os, math
 
 class node:
     def __init__(self, lchild = None, rchild = None, freq = 0, father = None, name = None):
+        '''建立節點之左右子節點、出現頻率(或其子節點總和)、父節點'''
         self.lchild = lchild
         self.rchild = rchild
         self.freq = freq
         self.father = father
         self.name = name
     def isLeft(self):
+        #判斷該節點是否為其父節點之左子節點
         return self.father.lchild == self
     def __str__(self):
         return f"lchild: {self.lchild}, rchild: {self.rchild}, freq: {self.freq}, father: {self.father}, name: {self.name}"
 
 def find_freq(fname):
+    '''計算文檔內所有文字出現次數'''
     with open(fname, "r", encoding="UTF-8") as f:
         text = f.read()
     dic = dict()
@@ -24,12 +27,14 @@ def find_freq(fname):
     return sorted(dic.items(), key=lambda x:x[1])
 
 def create_leaf(freq_dic):
+    '''將文字出現頻率建為樹葉節點'''
     node_list = []
     for nodes in freq_dic:
         node_list.append(node(name = nodes[0], freq = nodes[1]))
     return node_list
 
 def find_pos(L, value):
+    '''利用二分搜尋尋找某值應插入之位置'''
     i = 0
     j = len(L)
     while i < j:
@@ -40,9 +45,10 @@ def find_pos(L, value):
             j = mid - 1
         else:
             i = mid + 1
-    return (i+j)//2
+    return i
 
 def create_tree(node_list):
+    '''建立huffman tree'''
     L = node_list.copy()
     while len(L) > 1:
         lchild = L.pop(0)
@@ -54,22 +60,25 @@ def create_tree(node_list):
     return L[0]
 
 def huffman_encoding(node_list, root):
-    code = [''] * len(node_list)
+    '''由各節點出發，往根節點前進並建立編碼'''
+    code = [""] * len(node_list)
     for i in range(len(node_list)):
         temp = node_list[i]
         while temp != root:
             if temp.isLeft():
-                code[i] = '0' + code[i]
+                code[i] = "0" + code[i]
             else:
-                code[i] = '1' + code[i]
+                code[i] = "1" + code[i]
             temp = temp.father
     return code
 
 def print_code_table(code, freq_dic):
+    '''印出編碼表'''
     for i in range(len(code)):
         print("character: ", freq_dic[i][0], "frequency: ", freq_dic[i][1], "code: ", code[i])
 
 def txt_to_code(fname, code_dic):
+    '''將文檔改寫為對應之編碼'''
     with open(fname, "r", encoding="UTF-8") as f:
         text = f.read()
     out = []
@@ -78,6 +87,7 @@ def txt_to_code(fname, code_dic):
     return "".join(out)
 
 def code_to_file(new_fname, txt_code):
+    '''將編碼寫至bin檔案中'''
     binary_list = []
     while len(txt_code) > 8:
         binary_list.append(txt_code[:8])
